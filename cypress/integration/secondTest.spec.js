52209d462e6bf80e976da223e7c10819662db144
Gareth <grgreatrex@hotmail.com>
2021-03-08 13:46:17 +0000

    check boxes, drop down list and radio buttons

--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -77,7 +77,7 @@ describe('our second test suite', () => {
 
     })
 
-    it.only('assert property on date picker', () => {
+    it('assert property on date picker', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
@@ -88,8 +88,90 @@ describe('our second test suite', () => {
                 cy.get('nb-calendar-day-picker')
                     .contains('17')
                     .click()
-                
-                    cy.wrap(input).invoke('prop', 'value').should('contain', 'Mar 17, 2021')
+
+                cy.wrap(input).invoke('prop', 'value').should('contain', 'Mar 17, 2021')
             })
+    })
+
+    it('radio button tests', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+            cy.wrap(radioButtons)
+                .first()
+                // force true overrides the fact the radio was not visible
+                .check({ force: true })
+                .should('be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({ force: true })
+
+            cy.wrap(radioButtons)
+                .first()
+                // force true overrides the fact the radio was not visible
+                .check({ force: true })
+                .should('not.be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled')
+        })
+    })
+
+    it('check boxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        // cy.get('[type="checkbox"]').check({force: true})
+
+        cy.get('[type="checkbox"]').eq(0).click({ force: true });
+        //check does not uncheck!
+        cy.get('[type="checkbox"]').eq(1).check({ force: true });
+    })
+
+    it.only('lists and drop down elements', () => {
+        cy.visit('/')
+        //select the list
+        cy.get('nav nb-select').click()
+        //check list contains an item and can be selected
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+        //colours cypress works with RBG format and not hex
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+        //loop through and assert all options
+        cy.get('nav nb-select').then(dropdown => {
+            cy.wrap(dropdown).click()
+            
+            //get all options from list using for each loop
+            cy.get('.options-list nb-option').each((listItem, index) => {
+                const itemText = listItem.text().trim()
+                
+                // create list of colors for assertion later
+                const colors = {
+                    "Light": "rgb(255, 255, 255)",
+                    "Dark": "rgb(34, 43, 69)",
+                    "Cosmic": "rgb(50, 50, 89)",
+                    "Corporate": "rgb(255, 255, 255)"
+                }
+                //click on the listitem
+                cy.wrap(listItem).click()
+                //assert the list contains
+                cy.wrap(dropdown).should('contain', itemText)
+                // assert page changes color, using the key of the item name in the color list we created
+                cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+                
+                //set an index earlier so we can skip the final drop down click at the end of the test as it is not required.
+                if (index < 3) {
+                    cy.wrap(dropdown).click()
+                }
+            })
+        })
+
     })
 })
